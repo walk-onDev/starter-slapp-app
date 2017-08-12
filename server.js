e557e5cbecd7969ca0637fb6a529155fd02f27fa
Admin <Admin@Admins-MacBook-Pro.local>
2017-08-12 14:41:14 -0400

    added in handler for custom message

--- server.js
+++ server.js
@@ -34,6 +34,10 @@ slapp.message('help', ['mention', 'direct_message'], (msg) => {
   msg.say(HELP_TEXT)
 })
 
+slap.message('echo', ['mention', 'direct_message'], (msg) => {
+  msg.say('this is an example app created using slapp by walk-onDev')
+})
+
 // "Conversation" flow that tracks state - kicks off when user says hi, hello or hey
 slapp
   .message('^(hi|hello|hey)$', ['direct_mention', 'direct_message'], (msg, text) => {
