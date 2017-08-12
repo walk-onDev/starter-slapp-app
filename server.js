3a33e25aa70f40dc6e72ff79c7ec074fd6ff51d1
Admin <Admin@Admins-MacBook-Pro.local>
2017-08-12 14:09:37 -0400

    First commit

--- server.js
+++ server.js
@@ -34,6 +34,11 @@ slapp.message('help', ['mention', 'direct_message'], (msg) => {
   msg.say(HELP_TEXT)
 })
 
+//First test message
+slapp.message('echo', ['mention', 'direct_message'], (msg) => {
+  msg.say('Welcome to walk-onDevs slack app')
+})
+
 // "Conversation" flow that tracks state - kicks off when user says hi, hello or hey
 slapp
   .message('^(hi|hello|hey)$', ['direct_mention', 'direct_message'], (msg, text) => {
