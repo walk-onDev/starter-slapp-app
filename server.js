e16ea95170e6f332b8d8ded1aa58b7fd9d7c6b84
Admin <Admin@Admins-MacBook-Pro.local>
2017-08-13 15:01:46 -0400

    added dependancies

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const Slapp = require('slapp')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 const ConvoStore = require('slapp-convo-beepboop')
 const Context = require('slapp-context-beepboop')
 
