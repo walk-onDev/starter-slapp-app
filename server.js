e541453bb4b1e361833f3f104b87431224b6d46a
Admin <Admin@Admins-MacBook-Pro.local>
2017-08-15 21:21:26 -0400

    cleaning up a few things

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
 'use strict'
 
-const express = require('express')
-const Slapp = require('slapp')
-const ConvoStore = require('slapp-convo-beepboop')
-const Context = require('slapp-context-beepboop')
+const express = require('express');
+const Slapp = require('slapp');
+const ConvoStore = require('slapp-convo-beepboop');
+const Context = require('slapp-context-beepboop');
 
 // use `PORT` env var on Beep Boop - default to 3000 locally
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3000;
 
 var slapp = Slapp({
   // Beep Boop sets the SLACK_VERIFY_TOKEN env var
   verify_token: process.env.SLACK_VERIFY_TOKEN,
   convo_store: ConvoStore(),
   context: Context()
-})
+});
 
 var HELP_TEXT = `
 I will respond to the following messages:
 \`help\` - to see this message.
-\`hi\` - to demonstrate a conversation that tracks state.
-\`thanks\` - to demonstrate a simple response.
-\`<type-any-other-text>\` - to demonstrate a random emoticon response, some of the time :wink:.
-\`attachment\` - to see a Slack attachment message.
-`
+\`The only tool available in this app right now is the messagemany slash command
+`;
 
 //*********************************************
 // Setup different handlers for messages
@@ -30,41 +27,37 @@ I will respond to the following messages:
 
 // response to the user typing "help"
 slapp.message('help', ['mention', 'direct_message'], (msg) => {
-  msg.say(HELP_TEXT)
-})
-
+  msg.say(HELP_TEXT);
+});
 
 slapp.command('/messagemany', '(.*)', (msg, text, question) => {
-  // if "/inorout create Who is in?" is received: 
-  // text = create Who is in? 
-  // question = Who is in? 
-  
-  msg.say('Which channel(s) would you like to post to?').route('handleWhichChannels',{ message: text })
-})
+  msg.say('Which channel(s) would you like to post to?').route('handleWhichChannels',{ message: text });
+});
 
 slapp.route('handleWhichChannels', (msg, state) => {
+    var arr = msg.channelsMentioned();
 
-    var arr = msg.channelsMentioned()
     if (arr.length === 0) {
-      msg.say('Invalid response. Please give me a list of channel(s) to message.').route('handleWhichChannels', state)
-      return
+      msg.say('Invalid response. Please give me a list of channel(s) to message.').route('handleWhichChannels', state);
+      return;
     }
+
     for (var i = 0; i < arr.length; i++) {
-          msg.say({
-          text: state.message,
-            channel: arr[i]
-          })
-      }  
-})
+      msg.say({
+      text: state.message,
+        channel: arr[i]
+      });
+    }  
+});
 
 // attach Slapp to express server
-var server = slapp.attachToExpress(express())
+var server = slapp.attachToExpress(express());
 
 // start http server
 server.listen(port, (err) => {
   if (err) {
-    return console.error(err)
+    return console.error(err);
   }
 
-  console.log(`Listening on port ${port}`)
+  console.log(`Listening on port ${port}`);
 })
